e02bbc35890c8197a67950e626daa3998372c55f
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2025-02-20 16:10:07 -0500

    Delete src/main/java/com/rosinrevamp directory

--- src/main/java/com/rosinrevamp/RosinBoatInterface.java
+++
@@ -1,9 +0,0 @@
-package com.rosinrevamp;
-
-import net.minecraft.util.math.Vec3d;
-
-public interface RosinBoatInterface {
-	public void breakBoat();
-	public void breakWithVelocity(double x, double y, double z);
-	public void breakWithVelocity(Vec3d velocity);
-}
